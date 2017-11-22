2d4106579fce9c6de46ece06706ef991036ac9d2
Indrega <indroforconi@gmail.com>
2017-11-22 05:28:46 +0100

    change

---
+++ Forconi_Verifica.cpp
@@ -0,0 +1,17 @@
+// Verifica_Forconi.cpp : definisce il punto di ingresso dell'applicazione console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	for (int i = 0; i<10; i++) {
+		cout << i;
+	}
+	system("pause");
+	return 0;
+}
+
